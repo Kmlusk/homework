2d71a9d6985b0cb3fb2b2ef345ac3d561ff4b9fc
Kmlusk <140023945+Kmlusk@users.noreply.github.com>
2023-08-07 19:52:00 -0700

    20loop.py

--- 20loop.py
+++ 20loop.py
@@ -5,6 +5,15 @@
 # Hint: use the 3 argument form of range()
 
 # Your code goes here
+start = 5
+end = 50
+
+if start % 3 != 0:
+	for num in range(start, end +2, 3):
+		print(num, end=" ")
+else:
+	for num in range(start +2, end +2, 3):
+		print(num, end=" ")
 
 """
 python3 20loop.py
